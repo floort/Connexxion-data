300f177117b5fe96248f03f4f4c7dcb39b4319d9
Floor Terra <floort@gmail.com>
2011-01-13 14:41:36 +0100

    Added code to get all lines for a given stop.

--- scraper.py
+++ scraper.py
@@ -3,6 +3,16 @@
 import urllib
 import urllib2
 
+def get_aspx_fields(page):
+	fields = {}
+	start = page.find("<input type=\"hidden\" name=\"__VIEWSTATE\" id=\"__VIEWSTATE\" value=\"") + 64
+	end = page.find("\" />", start)
+	fields["__VIEWSTATE"] = page[start:end]
+	start = page.find("<input type=\"hidden\" name=\"__EVENTVALIDATION\" id=\"__EVENTVALIDATION\" value=\"") + 76
+	end = page.find("\" />", start)
+	fields["__EVENTVALIDATION"] = page[start:end]
+	return fields
+
 def halte_info(lsn, h, r):
 	page = urllib2.urlopen("http://www.connexxion.nl/dashboard/Halteinfo.aspx?lsn=%s&r=%s&h=%s"%(lsn, h, r)).read()
 	# Haal alle haltes op van deze pagina's
@@ -18,31 +28,47 @@ def halte_info(lsn, h, r):
 
 def get_lines(h):
 	page = urllib2.urlopen('http://www.connexxion.nl/dashboard/Default.aspx').read()
-	start = page.find("<input type=\"hidden\" name=\"__VIEWSTATE\" id=\"__VIEWSTATE\" value=\"") + 64
-	end = page.find("\" />", start)
-	viewstate = page[start:end]
-	start = page.find("<input type=\"hidden\" name=\"__EVENTVALIDATION\" id=\"__EVENTVALIDATION\" value=\"") + 76
-	end = page.find("\" />", start)
-	eventvalidation = page[start:end]
+	fields = get_aspx_fields(page)
 	eventtarget = "ctl00$mainContent$btnHalte_Zoekhalte"
 	page = urllib2.urlopen('http://www.connexxion.nl/dashboard/Default.aspx', urllib.urlencode((
-		('__VIEWSTATE', viewstate),
-		('__EVENTVALIDATION', eventvalidation),
+		('__VIEWSTATE', fields["__VIEWSTATE"]),
+		('__EVENTVALIDATION', fields["__EVENTVALIDATION"]),
 		('__EVENTTARGET', eventtarget),
 		('__EVENTARGUMENT', ''),
 		('__VIEWSTATEENCRYPTED', ''),
 		('rblHalteZoekop', 'Halte'),
-		#('ctl00$mainContent$ddHalte_Reisdatum', '13-1-2011'),
 		('ctl00$mainContent$tbHalte_Halte', 'blok'),
-		#('ctl00$mainContent$tbLijn_Uur', '01'),
-		#('ctl00$mainContent$tbLijn_Minuut', '43'),
-		#('ctl00$mainContent$ddLijn_Reisdatum', '13-1-2011'),
 	))).read()
-	return page
+	fields = get_aspx_fields(page)
+	page = urllib2.urlopen("http://www.connexxion.nl/dashboard/Halteinfo.aspx", urllib.urlencode((
+		('__VIEWSTATE', fields["__VIEWSTATE"]),
+		('__EVENTVALIDATION', fields["__EVENTVALIDATION"]),
+		('__EVENTTARGET', 'ctl00$mainContent$btnToonLijnlijst'),
+		('__EVENTARGUMENT', ''),
+		('__VIEWSTATEENCRYPTED', ''),
+		('ctl00$mainContent$ddHaltes', h),
+	))).read()
+	start_select = page.find('name="ctl00$mainContent$lbLijnen"')
+	end_select = page.find('</select>', start_select)
+	options = page[start_select:end_select]
+	lines = {}
+	print options
+	while True:
+		start = options.find("value=\"") + 7
+		if start == 6: break # -1 + 7 == 6
+		end = options.find("\">", start)
+		id = options[start:end]
+		start = options.find(">", end) + 1
+		end = options.find("</option>", start)
+		name = options[start:end]
+		lines[id] = name
+		options = options[end:]
+
+	return lines
 
 
 
 if __name__ == "__main__":
-	print get_lines("50320270")
+	print get_lines("37323930")
 
 
